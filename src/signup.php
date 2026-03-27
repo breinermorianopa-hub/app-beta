4b80374f2ca47da438c03cad88f802105746687f
breiner <breinermorianopa@gmail.com>
2026-03-27 12:02:20 -0500

    Unique mphone validation

--- src/signup.php
+++ src/signup.php
@@ -9,6 +9,14 @@ $m_phone = $_POST['mphone'] ;
 $p_sswd  = $_POST['passwd'] ;
 $enc_pass = md5($p_sswd);
 //Query to insert into SQL
+// Verificar si el teléfono ya existe
+$checkPhone = "SELECT * FROM USERS WHERE mobile_phone = '$m_phone'";
+$resultPhone = pg_query($local_conn, $checkPhone);
+
+if (pg_num_rows($resultPhone) > 0) {
+    echo "Error: el número de teléfono ya está registrado";
+    exit;
+}
 $sql = " INSERT INTO USERS (firstname,lastname,email,mobile_phone, passwd) 
 values ('$f_name', '$l_name','$e_mail','$m_phone','$enc_pass')" ;
 //execute query
